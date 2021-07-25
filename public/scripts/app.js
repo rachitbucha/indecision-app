06fc040e75f19e03b267ca886eed87843834b4a2
rachit.j <rachit.j@neodove.com>
2021-07-25 20:12:50 +0530

    Setting Up Babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,6 @@
+"use strict";
+
 console.log('App.js is running');
-
-
-var template = React.createElement("p", null, "Hello React");
+var template = /*#__PURE__*/React.createElement("p", null, "Setting Up Babel, Hello Babel && React");
 var appRoot = document.getElementById('app');
-
 ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('App.js is running');
+
+
+var template = <p>Setting Up Babel, Hello Babel && React</p>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
